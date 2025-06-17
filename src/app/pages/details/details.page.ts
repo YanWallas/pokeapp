226d9e37ea8ad81c385f7f6c96d954732ad440e6
Yan Wallas <yanwallas98@gmail.com>
2025-06-17 14:43:08 -0300

    ajustando a pagina de detalhes

--- src/app/pages/details/details.page.ts
+++ src/app/pages/details/details.page.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, map, switchMap, tap } from 'rxjs';
+import { Pokemon } from '../../interfaces/pokemon.interface';
+import { PokemonService } from '../../services/pokemon.service';
+import { FavoritesService } from '../../services/favorites.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.page.html',
   styleUrls: ['./details.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule]
+  imports: [IonicModule, CommonModule, RouterModule]
 })
 export class DetailsPage implements OnInit {
-  constructor() { }
-  ngOnInit() { }
+  pokemon$!: Observable<Pokemon>;
+  error: string | null = null;
+  isLoading = true;
+
+  constructor(
+    private route: ActivatedRoute,
+    private pokemonService: PokemonService,
+    private favoritesService: FavoritesService
+  ) { }
+
+  ngOnInit() {
+    this.pokemon$ = this.route.paramMap.pipe(
+      map(params => Number(params.get('id'))),
+      switchMap(id => this.pokemonService.getPokemonById(id)),
+      tap({
+        next: () => this.isLoading = false,
+        error: (error) => {
+          this.error = error;
+          this.isLoading = false;
+        }
+      })
+    );
+  }
+
+  isFavorite(id: number): boolean {
+    return this.favoritesService.isFavorite(id);
+  }
+
+  toggleFavorite(pokemon: Pokemon): void {
+    if (this.isFavorite(pokemon.id)) {
+      this.favoritesService.removeFavorite(pokemon.id);
+    } else {
+      this.favoritesService.addFavorite(pokemon);
+    }
+  }
 }
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, catchError, map, throwError } from 'rxjs';
+import { Pokemon, PokemonResponse } from '../interfaces/pokemon.interface';
 
 const API_URL = 'https://pokeapi.co/api/v2';
 
@@ -11,19 +12,51 @@ export class PokemonService {
 
   constructor(private http: HttpClient) { }
 
-  getPokemons() {
-    return this.http.get(`${API_URL}/pokemon?limit=151`).pipe(
-      map((response: any) => {
-
-        const results = response.results;
-
-        return results.map((pokemon: any, index: number) => {
+  getPokemons(): Observable<Pokemon[]> {
+    return this.http.get<PokemonResponse>(`${API_URL}/pokemon?limit=151`).pipe(
+      map((response) => {
+        return response.results.map((pokemon, index) => {
           const id = index + 1;
           const image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
 
           return { ...pokemon, id, image };
         });
-      })
+      }),
+      catchError(this.handleError)
     );
+  }
+
+  getPokemonById(id: number): Observable<Pokemon> {
+    return this.http.get<any>(`${API_URL}/pokemon/${id}`).pipe(
+      map((response) => {
+        const pokemon: Pokemon = {
+          id: response.id,
+          name: response.name,
+          image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${response.id}.png`,
+          url: `${API_URL}/pokemon/${response.id}`,
+          types: response.types.map((t: any) => t.type.name),
+          height: response.height,
+          weight: response.weight,
+          stats: response.stats.map((s: any) => ({
+            name: s.stat.name,
+            value: s.base_stat
+          }))
+        };
+        return pokemon;
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage = 'An error occurred';
+    if (error.error instanceof ErrorEvent) {
+      // Client-side error
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // Server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError(() => errorMessage);
   }
 }
